0023d05ff2f70592c7855f903046ca45f5eea8c8
Dmitriy Sobolev <stv2509@gmail.com>
2019-02-14 11:40:20 +0300

    Added informativeness in scripts

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+echo "Start install app"
+
 git clone https://github.com/stv2509/infra.git
 cd infra && bundle install
 puma -d
+
+if ps aux | grep puma | grep -v grep
+then
+	echo "SUCCESS: Reddit application up and running"
+else
+	echo "ERROR: Reddit application failed to start"
+fi
--- install_ruby.sh
+++ install_ruby.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Start Install Ruby"
+
 gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -sSL https://get.rvm.io | bash -s stable
 source /home/appuser/.rvm/scripts/rvm
@@ -7,5 +9,19 @@ rvm requirements
 rvm install 2.4
 rvm use 2.4.5 --default
 gem install bundler -V --no-ri --no-rdoc
-ruby -v
-bundle -v
+
+if ruby -v | grep "ruby 2.4"
+then
+	echo "SUCCESS: Ruby Installed"
+else
+	echo "ERROR: Ruby was not installed or version is wrong"
+fi
+
+if bundler -v | grep "Bundler version 1.11"
+then
+	echo "SUCCESS: Bundler Installed"
+else
+	echo "ERROR: Bundler was not installed or version is wrong"
+fi
+
+echo "Finish Install Ruby"
