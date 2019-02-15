fd62887f5e42d7d334b73eb87d1d51ba4bb531df
stv2509 <47211955+stv2509@users.noreply.github.com>
2019-02-15 16:31:39 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-echo "Start install app"
+sudo su - appuser
+cd /home/appuser/
+echo "user:"  $(whoami)  >> /home/appuser/install.log
+echo "pwd:"  $(pwd) >> /home/appuser/install.log
+
+echo "###############- Start install app -###############" >> /home/appuser/install.log
 
 git clone https://github.com/stv2509/infra.git
 cd infra && bundle update sinatra && bundle install
@@ -8,7 +13,7 @@ puma -d
 
 if ps aux | grep puma | grep -v grep
 then
-	echo "SUCCESS: Reddit application up and running"
+	echo "SUCCESS: Reddit application up and running" >> /home/appuser/install.log
 else
-	echo "ERROR: Reddit application failed to start"
+	echo "ERROR: Reddit application failed to start" >> /home/appuser/install.log
 fi
